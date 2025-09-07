232c9f19996103699cb1adb7b0bd5119178de491
Robson Junior <robsonjunior1994@gmail.com>
2025-09-07 01:20:12 -0300

    Terminando de desenhar as páginas principais

---
+++ Helps/Enumeradores.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pdvApp.Helps
+{
+    internal enum Enumeradores
+    {   Ativos,
+        Inativos, 
+        F,
+        J,
+        Admin,
+        Caixa,
+        Hoje,
+        Semana,
+        Mes,
+        Concluidas,
+        Canceladas
+    }
+}
